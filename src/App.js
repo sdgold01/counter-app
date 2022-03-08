1dc915c9a6d67644d6e38d0a68a52a62b333a887
__sdgold <engrsd22@gmail.com>
2022-03-08 19:24:02 +0100

    counter-app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <Counter initialNumber ={0} />
+        </div>
       </header>
     </div>
   );
